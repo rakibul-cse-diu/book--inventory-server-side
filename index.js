8832132bd1c470b980e7438b244261b783188aba
rakibul-cse-diu <rakibul2180@gmail.com>
2022-05-02 13:05:42 +0600

    connect to mongoDB

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@node-mongo-server-1.pkxfn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("books-inventory").collection("books");
+
+    if (err) {
+        console.log("IN THE", err)
+    } else {
+        console.log("DB connected");
+    }
+    // perform actions on the collection object
+    client.close();
+});
+
+
 app.get('/', (req, res) => {
     res.send("hellow books");
 })
