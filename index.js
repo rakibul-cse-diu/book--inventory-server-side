8155e562bf12dcd3ca0b9dfac3f509c86a9e90eb
rakibul-cse-diu <rakibul2180@gmail.com>
2022-05-02 14:30:34 +0600

    create api for get all books in inventory

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -8,21 +9,25 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@node-mongo-server-1.pkxfn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("books-inventory").collection("books");
+async function run() {
+    try {
+        await client.connect();
+        const booksCollection = client.db('books-inventory').collection('books')
 
-    if (err) {
-        console.log("IN THE", err)
-    } else {
-        console.log("DB connected");
+        app.get('/books', async (req, res) => {
+            const query = {};
+            const cursor = booksCollection.find(query);
+            const books = await cursor.toArray();
+            res.send(books);
+        })
     }
-    // perform actions on the collection object
-    client.close();
-});
+    finally {
 
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send("hellow books");
